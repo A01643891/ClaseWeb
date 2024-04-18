948864fd275338563b9125294cad2b61be868570
Jacob <a01643891@tec.mx>
2024-04-17 20:57:47 -0600

    Avance de login y empezar dashboard

--- React_spotify/music-app/src/components/Dashboard/Dashboard.jsx
+++ React_spotify/music-app/src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,81 @@
 
 const Dashboard = () =>{
     return (
-        <div>Dashboard</div>
+        <div className="flex justify-center items-center flex-col">
+            <div className=" m-4 text-4xl">
+                Spotify Music
+            </div>
+            <div className="m-2 border-black border-2">
+                <input className="px-2 py-1 " type="text"
+                name="song"
+                placeholder="Look for a song"
+                />
+            </div>
+            <div className="flex flex-col">
+                <div className="flex flex-row">
+                    <div className="text-2xl m-2">
+                        1.
+                    </div>
+                    <div className="flex flex-col">
+                        <div className="text-xl">
+                            Hail to the King
+                        </div>
+                        
+                        <div>
+                            Avenged Sevenfold
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-row">
+                    <div className="text-2xl m-2">
+                        2.
+                    </div>
+                    <div className="flex flex-col">
+                        <div className="text-xl">
+                            Bury the Light
+                        </div>
+                        
+                        <div>
+                            Casey Edwards, Victor Borba
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-row">
+                    <div className="text-2xl m-2">
+                        3.
+                    </div>
+                    <div className="flex flex-col">
+                        <div className="text-xl">
+                            Bfg Division
+                        </div>
+                        
+                        <div>
+                            Mick Gordon
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-row">
+                    <div className="text-2xl m-2">
+                        4.
+                    </div>
+                    <div className="flex flex-col">
+                        <div className="text-xl">
+                            This Fire
+                        </div>
+                        
+                        <div>
+                            Franz Ferdinand
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+            
+        </div>
     )
 }
 
--- React_spotify/music-app/src/components/Login/Login.jsx
+++ React_spotify/music-app/src/components/Login/Login.jsx
@@ -1,17 +1,75 @@
+import { useState } from "react";
+import { fetchSpotifyApi } from "../../api/spotifyAPI";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () =>{
+
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    })
+
+    const navigate = useNavigate();
+
+    const handleOnChange = (e) =>{
+        //console.log(e.target.value);
+        const newValues = {
+            ...form,
+            [e.target.name]: e.target.value,
+        }
+        console.log(newValues);
+        setForm(newValues);
+    }
+
+    const handleLogin = async() => {
+        const client_id = '4c01032e74f64dddb94629ece03041e1';
+        const client_secret = 'ce9819c8ede940a2b74e3da76374ed57';
+        const url = 'https://accounts.spotify.com/api/token'
+        const body = 'grant_type=client_credentials';
+        const token = 'Basic ' + btoa(client_id + ':' + client_secret);
+
+        const response = await fetchSpotifyApi(
+            url,
+            'POST',
+            body,
+            'application/x-www-form-urlencoded',
+            token
+        );
+
+        console.log(response);
+        localStorage.setItem('token', response.access_token);
+        navigate('/dashboard');
+    }
+
+    
+
     return (
         <div className="h-screen bg-slate-800 flex justify-center items-center flex-col">
             <div className="text-4xl text-white m-3  ">
                 Iniciar sesión en Spotify
             </div>
             <div className=" m-2 flex justify-center items-center flex-col">
-                <input className=" m-1" placeholder="Email:" type="text" name="email"/>
-                <input placeholder="Password:" type="password" name="password"/>
+                <input className=" m-1" 
+                type="text" 
+                name="email"
+                placeholder="Email:" 
+                value={form.email}
+                onChange={handleOnChange}/>
+
+                <input 
+                name="password"
+                placeholder="Password:"
+                value={form.password}
+                onChange={handleOnChange}
+                type="password" 
+                />
 
             </div>
             <div>
-            <button className=" p-1 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 rounded-md">Log In</button>
+            <button className=" p-1 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 rounded-md"
+            onClick={handleLogin}>
+                Log In
+            </button>
             </div>
             
 
