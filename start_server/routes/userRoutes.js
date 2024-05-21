79d5d57a6fa703964bd70849c41236924dba2bc5
Jacob <a01643891@tec.mx>
2024-05-20 20:22:45 -0600

    Added functions to get user by id, create and update users

--- start_server/routes/userRoutes.js
+++ start_server/routes/userRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUserByID);
+router.post('/', userController.createUser);
+router.put('/:id', userController.updateUser);
 
 module.exports = router;
